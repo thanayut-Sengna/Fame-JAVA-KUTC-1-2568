a5e2a69eb6cb9f7c28a0951c339a61ef1ffaecd6
Thanayut-Sengna <hesoyami08552gmali.com>
2025-06-13 11:58:19 +0700

    13/06/2568 update file helloworld and variabels งานอาจารย์เบนย์

---
+++ src/com/project/java_basic/HelloWorld.java
@@ -0,0 +1,9 @@
+package com.project.java_basic;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("HelloWorld");//คำสั่งการแสดงผล
+
+
+    }
+}
---
+++ src/com/project/java_basic/TypeVariables.java
@@ -0,0 +1,19 @@
+package com.project.java_basic;
+
+public class TypeVariables {
+    public static void main(String[] args) {
+        String  Name = "Thanayut";
+            System.out.println(Name);
+
+        int Number = 1213344111;
+            System.out.println(Number);
+
+        //float Numberf = (float)12.12;
+        double num =123.123;
+        System.out.println(num);
+        char Fame = 'a';
+        System.out.println(Fame);
+        boolean result = false;
+        System.out.println(Name+Number+ num+ Fame+ result+"เต่างับ");
+    }
+}
